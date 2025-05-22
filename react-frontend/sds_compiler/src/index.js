4525f6fa92b8186352b9841ad19f2e6f701d4f91
trulsrane <truls.rane@gmail.com>
2025-05-22 13:23:50 +0200

    Edit: frontend
    La till kodstruktur och css till figma-designen. Ligger kommenterat under orgiginalkoden

--- react-frontend/sds_compiler/src/index.js
+++ react-frontend/sds_compiler/src/index.js
@@ -15,3 +15,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+//Förslag
+//import React from 'react'
+//import ReactDOM from 'react-dom/client'
+//import App from './components/App.jsx'
+//import './index.css'
+
+//ReactDOM.createRoot(document.getElementById('root')).render(
+//    <React.StrictMode>
+//        <App />
+//    </React.StrictMode>
+//)
